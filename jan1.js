cc16435281cb8bec5b75341a124a30b022706fba
Avinash Goud <avinashgoud123456@gmail.com>
2026-01-01 03:11:27 +0530

    Add files via upload

---
+++ jan1.js
@@ -0,0 +1,13 @@
+var x=35;
+var y=56.65;
+var z="hello";
+var a=true;
+var b=null;
+var c=undefined;
+
+console.log(typeof x);
+console.log(typeof y);
+console.log(typeof z);
+console.log(typeof a);
+console.log("null result"+b);
+console.log("undefined"+c);
