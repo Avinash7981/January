6d16e240f658118a6a69671dc03c9ab768e30dbf
Avinash Goud <avinashgoud123456@gmail.com>
2026-01-08 09:12:53 +0530

    Add files via upload

---
+++ jan8.py
@@ -0,0 +1,9 @@
+
+# Create a list of 5 Places in Telengana and print it.
+L=["yadagirigutta","edupayal","swarnagiri","srisailam","ananthagirihills"]
+print(L)
+
+
+# Create a list of 5 Places in Telengana along with their distance from hyderabad and print it.
+L1=["yadagirigutta",100,"edupayal",200,"swarnagiri",300,"srisailam",400,"ananthagirihills",500]
+print(L1)
