34a4ccbc3c7f88150ba11bdc3926a2fca3c25d7f
Avinash Goud <avinashgoud123456@gmail.com>
2026-01-09 06:42:47 +0530

    Add files via upload

---
+++ jan9.py
@@ -0,0 +1,15 @@
+# Create a set of SSC subject marks
+ssc = {88,78,90,85,92,96}
+# Mathematics, Science, English, Social Studies, Telugu, Hindi.
+
+# Convert set to list using list constructor
+marks=(list(ssc))
+print(marks)
+
+                               
+# Create a set of SSC subject marks
+ssc1 = {88,78,90,85,92,96}
+# Mathematics, Science, English, Social Studies, Telugu, Hindi.
+
+# Convert set to list using list constructor
+print(ssc1)
