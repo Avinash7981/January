67f0d9d61657ab9457a204ab4b157b528c81abc0
Avinash Goud <avinashgoud123456@gmail.com>
2026-01-10 20:11:05 +0530

    Add files via upload

---
+++ jan10.py
@@ -0,0 +1,55 @@
+'''#Create a list of recently released movies
+L=['Dude','Mirai',''Kanthara-2'']
+print(L)
+
+#Create a list of recently released movies dates
+l1=["dude-2025","mirai-2025","kanthara-2025"]
+print(l1[0:])
+
+
+
+#create a list with even num from 2 to 20
+E = [2,4,6,8,10,12,14,16,18,20]
+#print list from 6 to 12
+print(E[2:6])
+#print from begin to 10
+print(E[0:5])
+#print from 8 to end of the list
+print(E[:10])
+
+
+#create a list multiples of 3
+M = [3,6,9,12,15,18,21,24,27,30]
+#modify the element multiples of 4 with number 4
+M[3]=4
+M[7]=4
+print(M)
+
+
+
+#create a list of first 10 natural number
+list1=[1,2,3,4,5,6,7,8,9,10]
+#Copy the above list to name list using copy method
+name=list1.copy()
+#by using list() create  a list variable rama 
+rama=list(name)
+#by using slicing[:] create a copy of Rama
+ramu=rama[:]
+#Print all the lists
+print(list1)        
+print(name)
+print(rama)
+print(ramu) 
+
+S={'red','green','blue','yellow'}
+S.remove('green')
+print(S)'''    
+
+
+
+#creating a list 
+a = (1,2,3,4,5)
+print(a)
+
+#converting list to tuple
+
