167a29ffc3f2e48f1d71e6280a030df4ca997c62
Avinash Goud <avinashgoud123456@gmail.com>
2026-01-11 10:23:31 +0530

    Add files via upload

---
+++ jan11.py
@@ -0,0 +1,20 @@
+
+#Create a list of recently released movies dates
+l1=["dude-2025","mirai-2025","kanthara-2025"]
+print(tuple(l1))
+
+l2=(181,182,183)
+print(list(l2))
+
+
+
+t1 = (1,2, [3, 4])
+t1[2][1]= 5
+print(t1)
+
+s= {1,2, 3}
+s.discard(2)
+print(s)
+s1={1,2,3,4}
+s1.discard(5)
+print(s1)
