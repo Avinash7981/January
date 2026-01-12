1c18dfa060e8f60b2380819e198282bed0339abe
Avinash Goud <avinashgoud123456@gmail.com>
2026-01-12 20:15:08 +0530

    Add files via upload

---
+++ jan12.py
@@ -0,0 +1,8 @@
+# Accept four numbers from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input ("Enter third number: "))
+num4 = float(input ("Enter fourth number: "))
+# Find and print the smallest number
+smallest = min(num1, num2, num3, num4)
+print("The smallest number is:", smallest)
