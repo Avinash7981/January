cef812578425e3477c61a54b1aa3769d26643147
Avinash Goud <avinashgoud123456@gmail.com>
2026-01-16 12:38:01 +0530

    Add files via upload

---
+++ jan16.py
@@ -0,0 +1,16 @@
+
+
+import maths.area as a
+print("Area of circle:", a.circle(5))
+print('Area of Recangle:', a.recangle(3,5))
+
+
+# from area import circle,recangle
+# print("Area of circle:" , circle(5))
+# print("Area of recangle:", recangle(2,4))
+
+
+
+# import area,perimeter
+# print("Area of circle:" , area.circle(5))
+# print("perimeter of circle:", perimeter.circle(6))
