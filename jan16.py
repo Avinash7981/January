0ff4d35b5866a2814b77ec5eeee87bdf311d0957
Avinash Goud <avinashgoud123456@gmail.com>
2026-01-16 12:40:05 +0530

    Uncomment area and perimeter function calls

--- jan16.py
+++ jan16.py
@@ -5,12 +5,12 @@ print("Area of circle:", a.circle(5))
 print('Area of Recangle:', a.recangle(3,5))
 
 
-# from area import circle,recangle
-# print("Area of circle:" , circle(5))
-# print("Area of recangle:", recangle(2,4))
+ from area import circle,recangle
+ print("Area of circle:" , circle(5))
+ print("Area of recangle:", recangle(2,4))
 
 
 
-# import area,perimeter
-# print("Area of circle:" , area.circle(5))
-# print("perimeter of circle:", perimeter.circle(6))
+ import area,perimeter
+ print("Area of circle:" , area.circle(5))
+ print("perimeter of circle:", perimeter.circle(6))
