197a55fb9cbc2f1e38c777fd10785cd8785bc1dc
Avinash Goud <avinashgoud123456@gmail.com>
2026-01-20 11:07:02 +0530

    Add files via upload

---
+++ jan20.py
@@ -0,0 +1,12 @@
+
+print("a" in "aeiou")
+print("b" in "aeiou")
+print("c" in "aeiou")
+print("d" in "aeiou")
+print("e" in "aeiou")
+
+print("a" not in "aeiou")
+print("b" not in "aeiou")
+print("c" not in "aeiou")
+print("d" not in "aeiou")
+print("e" not in "aeiou")
