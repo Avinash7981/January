b95ff66ee49ad1eaa2221b989ba2da5fb7ec961c
Avinash Goud <avinashgoud123456@gmail.com>
2026-01-22 00:37:38 +0530

    Add files via upload

---
+++ jan22.py
@@ -0,0 +1,11 @@
+name = "Ramana"
+age = 24
+salary = 240000.00
+place = "Hyderabad"
+print("Name:", name,"Type:", type(name))
+print("Age:", age,"type:", type(age))
+print("salary:", salary, "type", type(salary)) 
+print("Place:", place,"type:", type(place))
+
+
+
