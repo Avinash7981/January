ea95024da9d72535ecb89280ac09d4218044cd14
Avinash Goud <avinashgoud123456@gmail.com>
2026-01-21 11:52:22 +0530

    Add files via upload

---
+++ jan21.py
@@ -0,0 +1,41 @@
+#pizza party planning
+'''friends = 15
+slices_per_person = 3
+slices_per_pizza = 8
+pizza_cost = 996
+
+total_slices_needed = friends * slices_per_person
+pizzas_needed = total_slices_needed // slices_per_pizza  # Ceiling division
+total_cost = pizzas_needed * pizza_cost
+
+print("Pizzas needed:", pizzas_needed)
+print("Total cost:", total_cost)'''
+
+
+
+# Coffee Shop Profit Calculation
+'''
+cost_per_cup = 291
+sell_price_per_cup = 477
+cups_sold = 85
+
+profit_per_cup = sell_price_per_cup - cost_per_cup
+total_profit = profit_per_cup * cups_sold
+
+print("Profit per cup:", profit_per_cup)
+print("Total profit for 85 cups:", total_profit) '''
+
+
+# Social Media Engagement Calculation
+
+likes = 1250
+comments = 89
+shares = 34
+
+total_engagement = likes + comments + shares
+
+likes_percent = (likes / total_engagement) * 100
+comments_percent = (comments / total_engagement) * 100
+shares_percent = (shares / total_engagement) * 100
+
+print("Total engagement:", total_engagement)
